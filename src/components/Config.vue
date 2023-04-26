94b9f502c49495c05e9c0fd995e30a0d4a9a09d4
Cheuk Yin Ng <chucksys88@gmail.com>
2023-04-26 00:45:07 -0700

    feat: add config editing

---
+++ src/components/Config.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+import type { IConfig } from '../storage';
+
+defineProps<{
+  modelValue: IConfig,
+}>();
+
+defineEmits<{
+  (e: 'update:modelValue', newValue: IConfig): void
+}>();
+</script>
+
+<template>
+  <section>
+    Edit Config
+
+    <label for="autoSave">Auto-save?</label>
+    <input type="checkbox" name="autoSave" v-model="modelValue.autoSave">
+  </section>
+</template>
--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -4,6 +4,8 @@ import { ref, watch, reactive } from 'vue';
 import { useRoute } from 'vue-router';
 import { emptySaveData, saveToLocalStorage, loadDefaultSaveNameFromLocalStorage, loadSaveFromBase64, loadSaveFromLocalStorage } from '../storage';
 
+import Config from './Config.vue';
+
 const route = useRoute();
 
 const defaultSaveName = loadDefaultSaveNameFromLocalStorage();
@@ -26,16 +28,38 @@ if (queryB64) {
   }
 }
 
+let mode = ref('config');
+
 watch(workingData, async (newData, _) => {
   if (newData.config.autoSave) {
     lastSave.value = new Date();
     saveToLocalStorage(defaultSaveName, newData);
   }
-})
+});
 </script>
 
 <template>
-  <main></main>
+  <main>
+    <section v-if="mode === 'all'">
+      Edit the game
+    </section>
+
+    <section v-if="mode === 'question'">
+      Edit a question
+    </section>
+
+    <Config
+        v-if="mode === 'config'"
+        v-model="workingData.config" />
+
+    <section v-if="mode === 'load'">
+      Load save
+    </section>
+
+    <section v-if="mode == 'save'">
+      Save to
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
