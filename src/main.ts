b9205993435549448026c48e3a203f873939791c
Cheuk Yin Ng <chucksys88@gmail.com>
2025-09-29 00:29:30 -0700

    wip: materialize usage

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import 'materialize-css/dist/css/materialize.min.css';
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
