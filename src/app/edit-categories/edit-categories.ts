962182ecff9d2692622ac2bffcbb0461f088b9c6
Cheuk Yin Ng <chucksys88@gmail.com>
2025-10-20 23:36:13 -0700

    wip: click new category

--- src/app/edit-categories/edit-categories.ts
+++ src/app/edit-categories/edit-categories.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
 import { liveQuery } from 'dexie';
 import { from, Observable } from 'rxjs';
 
 import { Database, Category } from '../database';
 import { Config } from '../config';
 import { EditCategory } from '../edit-category/edit-category';
+import { EditCategoryDialog } from '../edit-category-dialog/edit-category-dialog';
 
 @Component({
   selector: 'app-edit-categories',
@@ -18,11 +20,23 @@ import { EditCategory } from '../edit-category/edit-category';
 export class EditCategories {
   categories$: Observable<Category[]>;
 
-  constructor(private db: Database, private config: Config) {
+  constructor(private db: Database, private config: Config, private dialog: MatDialog) {
     this.categories$ = from(liveQuery(
       () => db.categories.where({
         gameId: config.selectedGameId
       }).toArray()
     ));
+  }
+
+  handleClickNewCategory(e: Event) {
+    const dialogRef = this.dialog.open(EditCategoryDialog, {
+      data: {
+        name: '',
+        order: 0,
+      },
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
   }
 }
--- src/app/edit-category-dialog/edit-category-dialog.ts
+++ src/app/edit-category-dialog/edit-category-dialog.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, model, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatDialogTitle, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogTitle, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+
+export interface DialogData {
+  name: string;
+  order: number;
+}
 
 @Component({
   selector: 'app-edit-category-dialog',
@@ -12,6 +17,22 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './edit-category-dialog.scss'
 })
 export class EditCategoryDialog {
-  name = '';
-  order = 0;
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+  readonly name = model(this.data.name);
+  readonly order = model(this.data.order);
+
+  constructor(
+    private readonly dialogRef: MatDialogRef<EditCategoryDialog>,
+  ) {}
+
+  handleCreate() {
+    this.dialogRef.close({
+      name: this.name(),
+      order: this.order(),
+    });
+  }
+
+  handleCancel() {
+    this.dialogRef.close();
+  }
 }
