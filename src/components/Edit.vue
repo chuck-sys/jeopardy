de054b139bd5ee4dd1932dbff00067d7b7c9345d
Cheuk Yin Ng <chucksys88@gmail.com>
2026-01-13 00:46:40 -0800

    cleanup: rm query string reading feature

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
 import { ref, watch, reactive } from 'vue';
-import { useRoute } from 'vue-router';
 
 import Config from './Config.vue';
 import EditAllQuestions from './EditAllQuestions.vue';
 
-const route = useRoute();
-
-const queryB64 = route.query.data;
-
-/** Most recent time we saved to local storage */
-let lastSave = ref(new Date());
-
-// Query string blob takes precedent
-if (queryB64) {
-}
 </script>
 
 <template>
